4a621e391c17aa04e239fa50fc961241cd2b5a68
turnertrowbridge <ttrowbridge7322@sdsu.edu>
2024-06-01 13:45:59 -0700

    Fixed nickname being zipped with wrong list

--- deck.py
+++ deck.py
@@ -12,7 +12,7 @@ class Deck:
                  '9', '10', 'Jack', 'Queen', 'King', 'Ace']
         nicknames = ['2', '3', '4', '5', '6', '7', '8',
                      '9', '10', 'J', 'Q', 'K', 'A']
-        return [(rank, suit, nickname) for suit, nickname in zip(suits, nicknames) for rank in ranks]
+        return [(rank, suit, nickname) for suit in suits for rank, nickname in zip(ranks, nicknames)]
 
     def deal_card(self):
         return self.deck.pop()
