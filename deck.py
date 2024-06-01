230ed878de8874082d41d2bcd4084a64745cd25a
turnertrowbridge <ttrowbridge7322@sdsu.edu>
2024-06-01 13:28:16 -0700

    Added sprites and card value nicknames

--- deck.py
+++ deck.py
@@ -10,7 +10,9 @@ class Deck:
         suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
         ranks = ['2', '3', '4', '5', '6', '7', '8',
                  '9', '10', 'Jack', 'Queen', 'King', 'Ace']
-        return [(rank, suit) for suit in suits for rank in ranks]
+        nicknames = ['2', '3', '4', '5', '6', '7', '8',
+                     '9', '10', 'J', 'Q', 'K', 'A']
+        return [(rank, suit, nickname) for suit, nickname in zip(suits, nicknames) for rank in ranks]
 
     def deal_card(self):
         return self.deck.pop()
--- ui.py
+++ ui.py
@@ -13,10 +13,15 @@ def draw_button(screen, font, text, x, y, width, height, color=(255, 0, 0)):
 
 def draw_card(screen, font, card, x, y, card_color=(255, 255, 255), text_color=(0, 0, 0)):
     pygame.draw.rect(screen, card_color, (x, y, 100, 150))
-    draw_text(screen, font, card[1], x + 10, y + 50, text_color)
-    draw_text(screen, font, card[0], x + 10, y + 10, text_color)  # top left
+    # draw_text(screen, font, card[1], x + 10, y + 50, text_color)  # Suit name
+    draw_text(screen, font, card[2], x + 10, y + 10, text_color)  # top left
 
     # draw bottom right upside down
-    text_surface = font.render(card[0], True, text_color)
+    text_surface = font.render(card[2], True, text_color)
     flipped_text_surface = pygame.transform.flip(text_surface, False, True)
     screen.blit(flipped_text_surface, (x + 80, y + 130))  # bottom left
+
+    # Add sprite
+    card_sprite = pygame.image.load(f"./images/{card[1].lower()}.png")
+    card_sprite = pygame.transform.scale(card_sprite, (50, 50))
+    screen.blit(card_sprite, (x + 25, y + 50))
